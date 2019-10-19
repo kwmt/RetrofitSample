a9fa5b29ab841846b65b3f9702cfff15ade64de8
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-10-19 17:12:21 +0900

    String to List<Repo>

--- app/src/main/java/net/kwmt27/retrofitsample/MyConverterFactory.kt
+++ app/src/main/java/net/kwmt27/retrofitsample/MyConverterFactory.kt
@@ -1,8 +1,13 @@
 package net.kwmt27.retrofitsample
 
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonConfiguration
+import kotlinx.serialization.list
+import net.kwmt27.retrofitsample.data.model.Repo
 import okhttp3.ResponseBody
 import retrofit2.Converter
 import retrofit2.Retrofit
+import java.io.BufferedReader
 import java.lang.reflect.Type
 
 class MyConverterFactory : Converter.Factory() {
@@ -15,9 +20,20 @@ class MyConverterFactory : Converter.Factory() {
         return MyResponseBodyConverter()
     }
 
-    class MyResponseBodyConverter : Converter<ResponseBody, String> {
-        override fun convert(value: ResponseBody): String? {
-            return "test"
+    class MyResponseBodyConverter : Converter<ResponseBody, List<Repo>> {
+        override fun convert(value: ResponseBody): List<Repo>? {
+            val reader = BufferedReader(value.byteStream().reader())
+            val content = StringBuilder()
+            reader.use { reader ->
+                var line = reader.readLine()
+                while (line != null) {
+                    content.append(line)
+                    line = reader.readLine()
+                }
+            }
+
+            val json = Json { strictMode = false }
+            return json.parse(Repo.serializer().list, content.toString())
         }
     }
 }
--- app/src/main/java/net/kwmt27/retrofitsample/data/model/Repo.kt
+++ app/src/main/java/net/kwmt27/retrofitsample/data/model/Repo.kt
@@ -1,3 +1,6 @@
 package net.kwmt27.retrofitsample.data.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Repo(val name: String)
