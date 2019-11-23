d9d55530e7c84a1e8d24fbb4860062b4a1ac7c28
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-11-23 21:33:26 +0900

    AsyncTaskからcoroutineに変更

--- app/src/main/java/net/kwmt27/retrofitsample/FirstFragment.kt
+++ app/src/main/java/net/kwmt27/retrofitsample/FirstFragment.kt
@@ -1,6 +1,5 @@
 package net.kwmt27.retrofitsample
 
-import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -9,12 +8,21 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.list
 import net.kwmt27.retrofitsample.data.GitHubService
 import net.kwmt27.retrofitsample.data.model.Repo
 import retrofit2.Call
+import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.await
 import java.io.BufferedInputStream
+import java.io.BufferedReader
 import java.net.HttpURLConnection
 import java.net.URL
 
@@ -37,58 +45,83 @@ class FirstFragment : Fragment() {
         val resultTextView = view.findViewById<TextView>(R.id.textview_first)
 
         view.findViewById<Button>(R.id.retrofit_button).setOnClickListener {
-            val retrofit = Retrofit.Builder()
+            val gitHubService = Retrofit.Builder()
                 .baseUrl("https://api.github.com/")
                 .addConverterFactory(MyConverterFactory())
                 .build()
+                .create(GitHubService::class.java)
 
-            val gitHubService = retrofit.create(GitHubService::class.java)
-            val repos: Call<List<Repo>> = gitHubService.listRepos("kwmt")
-
-            repos.enqueue(object : retrofit2.Callback<List<Repo>> {
-                override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
-                    Log.d(TAG, t.message)
+            lifecycleScope.launch(Dispatchers.IO) {
+                val results = gitHubService.listRepos("kwmt").await()
+                results.forEach {
+                    Log.d(TAG, it.name)
                 }
-
-                override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
-                    Log.d(TAG, response.toString())
-                }
-            })
+            }
+//
+//            repos.enqueue(object : Callback<List<Repo>> {
+//                override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
+//                    Log.d(TAG, t.message)
+//                }
+//
+//                override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
+//                    Log.d(TAG, response.toString())
+//                }
+//            })
         }
 
         view.findViewById<Button>(R.id.HttpURLConnection_button).setOnClickListener {
-            RequestAsyncTask().execute()
-        }
-    }
-
-    private class RequestAsyncTask : AsyncTask<Unit, Unit, String?>() {
-        override fun doInBackground(vararg params: Unit?): String? {
-            Log.d(TAG, "doInbackground start")
-            val url = URL("https://api.github.com/users/kwmt/repos")
-            val urlConnection = url.openConnection() as HttpURLConnection
-            var result: String? = null
-            try {
-                val inputStream = BufferedInputStream(urlConnection.inputStream)
-                result = readStream(inputStream)
-            } finally {
-                urlConnection.disconnect()
-            }
-            return result
-        }
-
-        override fun onPostExecute(result: String?) {
-            super.onPostExecute(result)
-            Log.d(TAG, result)
-        }
-
-        private fun readStream(inputStream: BufferedInputStream): String {
-            return inputStream.bufferedReader().use {
-                it.readText()
+            val serviceWithoutRetrofit: GitHubServiceWithoutRetrofit = GitHubServiceImpl()
+            lifecycleScope.launch {
+                val results = serviceWithoutRetrofit.listRepos("kwmt")
+                results.forEach {
+                    Log.d(TAG, it.name)
+                }
             }
         }
     }
 
     companion object {
         private const val TAG = "GitHubSample"
+    }
+}
+
+interface GitHubServiceWithoutRetrofit {
+    suspend fun listRepos(user: String): List<Repo>
+}
+
+class GitHubServiceImpl : GitHubServiceWithoutRetrofit {
+    override suspend fun listRepos(user: String): List<Repo> {
+        return withContext(Dispatchers.IO) {
+            val url = URL("https://api.github.com/users/$user/repos")
+            val urlConnection = url.openConnection() as HttpURLConnection
+            val result: List<Repo>
+            try {
+                val inputStream = BufferedInputStream(urlConnection.inputStream)
+                result = readStream(inputStream)
+            } finally {
+                urlConnection.disconnect()
+            }
+            return@withContext result
+        }
+    }
+
+    private fun readStream(inputStream: BufferedInputStream): List<Repo> {
+        return inputStream.bufferedReader().use {
+            convert(it)
+        }
+    }
+
+    private fun convert(reader: BufferedReader): List<Repo> {
+        val content = StringBuilder()
+        reader.use {
+            var line = it.readLine()
+            while (line != null) {
+                content.append(line)
+                line = it.readLine()
+            }
+        }
+
+        val json = Json { strictMode = false }
+        return json.parse(Repo.serializer().list, content.toString())
     }
 }
